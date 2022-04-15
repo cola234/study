20d792ff90d8dbe4607c516a5eddf50eb2c0e9a6
cola233 <2414098984@qq.com>
2022-04-15 10:24:14 +0800

    数组指针 回调函数

--- test_4_12/test_4_12/test.c
+++ test_4_12/test_4_12/test.c
@@ -75,17 +75,119 @@
 //	return 0;
 //}
 
-char* my_strcpy(char* dest, const char* src)
-//1.写一个函数指针pf 指向my_strcpy
-//2.写一个函数指针数组 存放4个my_strcpy
-{
-	;
-}
+//char* my_strcpy(char* dest, const char* src)
+////1.写一个函数指针pf 指向my_strcpy
+////2.写一个函数指针数组 存放4个my_strcpy
+//{
+//	;
+//}
+//
+//int main()
+//{
+//	char* (*pf)(char*, const char*)=my_strcpy;//函数指针pf
+//	char* (*parr[4])(char*, const char*) = { my_strcpy,my_strcpy,my_strcpy,my_strcpy };//函数指针p
+//	printf("%s", parr[1]);
+//	return 0;
+//}
+//void menu()
+//{
+//	printf("***1.Add    2.Sub***\n");
+//	printf("***3.Mul    4.Div***\n");
+//	printf("***    0.exit    ***\n");
+//}
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int  Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()//函数指针的用法：计算器(转移表）
+//{
+//	int input = 0;
+//	int x = 0;
+//	int y = 0;
+//	int(*pfarr[5])(int, int) = { 0,Add,Sub,Mul,Div };
+//	do
+//	{
+//		menu();
+//		printf("请选择：>");
+//		scanf_s("%d", &input);
+//		printf("请输入两个操作数");
+//		scanf_s("%d%d", &x,&y);
+//		int ret = pfarr[input](x, y);
+//		printf("%d\n", ret);
+//	} while (input);
+//	return 0;
+//}
 
-int main()
+
+/*回调函数 函数指针使用方法之二*/
+
+void menu()
 {
-	char* (*pf)(char*, const char*)=my_strcpy;//函数指针pf
-	char* (*parr[4])(char*, const char*) = { my_strcpy,my_strcpy,my_strcpy,my_strcpy };//函数指针p
-	printf("%s", parr[1]);
+	printf("***1.Add    2.Sub***\n");
+	printf("***3.Mul    4.Div***\n");
+	printf("***    0.exit    ***\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int  Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+void Calc(int(*pf)(int,int))//传过来的是函数的地址 这里也用函数的地址做形参
+{
+	int x = 0;
+	int y = 0;
+	printf("请输入两个操作数\n");
+	scanf_s("%d%d\n", &x, &y);
+	printf("%d", pf(x, y));
+}
+int main()//函数指针的用法：计算器(转移表）
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 1:
+			Calc(Add);
+		case 2:
+			Calc(Sub);
+		case 3:
+			Calc(Mul);
+		case 4:
+			Calc(Div);
+			break;
+		case 0:
+			printf("退出\n");
+			break;
+		default:
+			printf("选择错误\n");
+		}
+	} while (input);
 	return 0;
 }
